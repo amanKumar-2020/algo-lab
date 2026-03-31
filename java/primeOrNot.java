30687808e419d02b2750342ca78ae137c29b8141
Aman kumar <jnvaman4@gmail.com>
2026-03-31 13:36:20 +0530

    Refactor prime number check to handle edge cases and improve efficiency

--- java/primeOrNot.java
+++ java/primeOrNot.java
@@ -2,26 +2,55 @@
 
 import java.util.Scanner;
 
+// public class primeOrNot {
+//     public static void main(String []args){
+//         Scanner scanner = new Scanner(System.in);
+
+//         System.out.println("Enter a number");
+//         int number = scanner.nextInt();
+//         boolean prime = true;
+
+//         for(int i = 2 ; i< number ; i++){
+//             if(number%i==0 && number >0){
+//                 prime = false;
+//                 break;
+//             }
+//         }
+
+//         if(prime){
+//             System.out.println("number is prime");
+//         }else{
+//             System.out.println("number is not prime");
+//         }
+//         scanner.close();
+//     }
+// }
 public class primeOrNot {
     public static void main(String []args){
         Scanner scanner = new Scanner(System.in);
 
         System.out.println("Enter a number");
         int number = scanner.nextInt();
-        boolean prime = false;
-
-        for(int i = 2 ; i< number ; i++){
-            if(number%i==0 && number >0){
-                prime = true;
-                break;
+       
+        if (number <=1) {
+            System.out.println("Number is neither prime nor composite");
+        }else{
+            boolean prime = true;
+            int c = 2;
+            while (c*c <= number) {
+                if(number % c ==0){
+                    prime = false;
+                    break;
+                }
+                c+= 1;
+            }
+            if (prime) {
+                System.out.println("number is prime");
+            } else {
+                System.out.println("number is not prime");
             }
         }
-
-        if(prime){
-            System.out.println("number is prime");
-        }else{
-            System.out.println("number is not prime");
-        }
+        
         scanner.close();
     }
 }
