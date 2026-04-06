8840b514d758b9647a999fdaff9bc63a0deb0317
Aman kumar <jnvaman4@gmail.com>
2026-04-06 21:47:12 +0530

    Add LargestElement.java to implement functionality for finding the largest element in an array

---
+++ java/LargestElement.java
@@ -0,0 +1,15 @@
+// Problem: Find Largest Element in Array (Java)
+
+/**
+ * Approach (Step-by-Step)
+ * Assume first element is the largest
+ * Loop through the array
+ * Compare each element with current max
+ * Update max if bigger value found
+ * Print result
+ * 
+ */
+
+public class LargestElement {
+    
+}
