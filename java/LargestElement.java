420a74e59203e73fb674bac166725b6ec3962c78
Aman kumar <jnvaman4@gmail.com>
2026-04-06 21:48:26 +0530

    LargestElement.java write logic

--- java/LargestElement.java
+++ java/LargestElement.java
@@ -11,5 +11,23 @@
  */
 
 public class LargestElement {
-    
+    public static void main(String[] args) {
+
+        int[] arr = { 10, 25, 5, 40, 15 };
+
+        // Step 1: Assume first element is maximum
+        int max = arr[0];
+
+        // Step 2: Traverse array
+        for (int i = 1; i < arr.length; i++) {
+
+            // Step 3: Compare
+            if (arr[i] > max) {
+                max = arr[i]; // Step 4: Update max
+            }
+        }
+
+        // Step 5: Print result
+        System.out.println("Largest element is: " + max);
+    }
 }
