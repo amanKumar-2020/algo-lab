b41e38b86a1ee77e194602f9106caff360cb0981
Aman kumar <jnvaman4@gmail.com>
2026-04-10 00:07:42 +0530

    Add Palindrome.java to implement functionality for checking palindrome numbers

---
+++ java/Palindrome.java
@@ -0,0 +1,37 @@
+// Problem: Check Palindrome Number
+
+import java.util.Scanner;
+
+/**
+ * Logic (Super Important)
+ * Store original number
+ * Reverse the number (same logic you learned 🔁)
+ * Compare:
+ * If original == reverse → Palindrome
+ * Else → Not palindrome
+ */
+public class Palindrome {
+     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int num = sc.nextInt();
+
+        int original = num;
+        int reverse = 0;
+
+        while (num != 0) {
+            int digit = num % 10;
+            reverse = reverse * 10 + digit;
+            num = num / 10;
+        }
+
+        if (original == reverse) {
+            System.out.println("Palindrome number");
+        } else {
+            System.out.println("Not a palindrome");
+        }
+
+        sc.close();
+    }
+}
