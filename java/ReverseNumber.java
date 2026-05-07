8d9f8b22045cd0ebf2c89d6ea4517f363d644d83
Aman kumar <jnvaman4@gmail.com>
2026-05-07 23:53:50 +0530

    Refactor ReverseNumber.java to implement moveZerosToEnd functionality and clean up main method

--- java/ReverseNumber.java
+++ java/ReverseNumber.java
@@ -1,20 +1,41 @@
-import java.util.Scanner;
+// Move all zeros to end
 
-public class ReverseNumber {
-     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+class MoveZeros {
 
-        System.out.print("Enter a number: ");
-        int num = sc.nextInt();
+    public static void moveZerosToEnd(int[] arr) {
 
-        int reversed = 0;
+        int index = 0;
 
-        while (num != 0) {
-            int digit = num % 10;        // get last digit
-            reversed = reversed * 10 + digit; // build reversed number
-            num = num / 10;              // remove last digit
+        // Move non-zero elements forward
+        for (int i = 0; i < arr.length; i++) {
+
+            if (arr[i] != 0) {
+                arr[index] = arr[i];
+                index++;
+            }
         }
 
-        System.out.println("Reversed number: " + reversed);
+        // Fill remaining positions with 0
+        while (index < arr.length) {
+            arr[index] = 0;
+            index++;
+        }
+    }
+
+    public static void main(String[] args) {
+
+        int[] arr = { 1, 0, 5, 0, 3, 0, 9 };
+
+        System.out.println("Original Array:");
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
+
+        moveZerosToEnd(arr);
+
+        System.out.println("\n\nArray After Moving Zeros:");
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
     }
 }
