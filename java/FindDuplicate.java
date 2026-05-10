6112861872b202ef307f0bd88b50a6dac49c445b
Aman kumar <jnvaman4@gmail.com>
2026-05-10 23:39:26 +0530

    Find Duplicate Number

--- java/FindDuplicate.java
+++ java/FindDuplicate.java
@@ -1,35 +1,3 @@
-<<<<<<< HEAD
-class FindDuplicate {
-
-    public static void findDuplicate(int[] arr) {
-
-        boolean found = false;
-
-        for (int i = 0; i < arr.length; i++) {
-
-            for (int j = i + 1; j < arr.length; j++) {
-
-                if (arr[i] == arr[j]) {
-                    System.out.println("Duplicate Element: " + arr[i]);
-                    found = true;
-                    break;
-                }
-            }
-        }
-
-        if (!found) {
-            System.out.println("No Duplicate Element Found");
-        }
-    }
-
-    public static void main(String[] args) {
-
-        int[] arr = {1, 3, 5, 2, 3, 7, 5};
-
-        findDuplicate(arr);
-    }
-}
-=======
 import java.util.HashSet;
 
 class FindDuplicate {
@@ -50,4 +18,4 @@ class FindDuplicate {
         }
     }
 }
->>>>>>> 705060f (Find Duplicate Number)
+
