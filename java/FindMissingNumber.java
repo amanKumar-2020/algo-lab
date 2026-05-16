b10fe49b3379da2d35730078b99933a754bf3f41
Aman Kumar <jnvaman4@gmail.com>
2026-05-16 20:09:35 +0530

    Create FindMissingNumber.java

---
+++ java/FindMissingNumber.java
@@ -0,0 +1,21 @@
+class FindMissingNumber {
+
+    public static void main(String[] args) {
+
+        int[] arr = {1, 2, 4, 5};
+
+        int n = 5;
+
+        int expectedSum = n * (n + 1) / 2;
+
+        int actualSum = 0;
+
+        for (int num : arr) {
+            actualSum += num;
+        }
+
+        int missingNumber = expectedSum - actualSum;
+
+        System.out.println("Missing Number: " + missingNumber);
+    }
+}
