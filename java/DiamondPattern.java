61219e12c76e4d5faa759a874c218cda2873a356
Aman Kumar <jnvaman4@gmail.com>
2026-05-19 19:44:08 +0530

    Diamond Star Pattern

---
+++ java/DiamondPattern.java
@@ -0,0 +1,39 @@
+class DiamondPattern {
+
+    public static void main(String[] args) {
+
+        int rows = 5;
+
+        // Upper Triangle
+        for (int i = 1; i <= rows; i++) {
+
+            // Print spaces
+            for (int j = 1; j <= rows - i; j++) {
+                System.out.print(" ");
+            }
+
+            // Print stars
+            for (int j = 1; j <= (2 * i - 1); j++) {
+                System.out.print("*");
+            }
+
+            System.out.println();
+        }
+
+        // Lower Triangle
+        for (int i = rows - 1; i >= 1; i--) {
+
+            // Print spaces
+            for (int j = 1; j <= rows - i; j++) {
+                System.out.print(" ");
+            }
+
+            // Print stars
+            for (int j = 1; j <= (2 * i - 1); j++) {
+                System.out.print("*");
+            }
+
+            System.out.println();
+        }
+    }
+}
