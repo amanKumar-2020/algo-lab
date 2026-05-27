e07b27ba378a06d381ebcf43439a42d80c6453c5
Aman Kumar <jnvaman4@gmail.com>
2026-05-27 12:49:17 +0530

    Factorial using Recursion

---
+++ java/FactorialRecursion.java
@@ -0,0 +1,23 @@
+class FactorialRecursion {
+
+    // Recursive method
+    static int factorial(int n) {
+
+        // Base condition
+        if (n == 0 || n == 1) {
+            return 1;
+        }
+
+        // Recursive call
+        return n * factorial(n - 1);
+    }
+
+    public static void main(String[] args) {
+
+        int num = 5;
+
+        int result = factorial(num);
+
+        System.out.println("Factorial of " + num + " is: " + result);
+    }
+}
