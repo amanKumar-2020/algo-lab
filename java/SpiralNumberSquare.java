141d48412b300eef39f7f5a4fd69d267ad050201
Aman Kumar <jnvaman4@gmail.com>
2026-06-24 23:36:14 +0530

    Spiral Number Square Pattern 🌀

---
+++ java/SpiralNumberSquare.java
@@ -0,0 +1,27 @@
+class SpiralNumberSquare {
+
+    public static void main(String[] args) {
+
+        int n = 4;
+
+        for (int i = 0; i < 2 * n - 1; i++) {
+
+            for (int j = 0; j < 2 * n - 1; j++) {
+
+                int top = i;
+                int left = j;
+                int right = (2 * n - 2) - j;
+                int bottom = (2 * n - 2) - i;
+
+                int min = Math.min(
+                        Math.min(top, bottom),
+                        Math.min(left, right)
+                );
+
+                System.out.print((n - min) + " ");
+            }
+
+            System.out.println();
+        }
+    }
+}
