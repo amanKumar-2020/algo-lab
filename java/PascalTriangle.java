c7aa9d9b6fd099952497e6b25ac3ade00915cacc
Aman Kumar <jnvaman4@gmail.com>
2026-08-26 22:47:15 +0530

    Pascal's triangle

---
+++ java/PascalTriangle.java
@@ -0,0 +1,27 @@
+class PascalTriangle {
+
+    public static void main(String[] args) {
+
+        int rows = 5;
+
+        for (int i = 0; i < rows; i++) {
+
+            // Print spaces
+            for (int j = 0; j < rows - i - 1; j++) {
+                System.out.print(" ");
+            }
+
+            int num = 1;
+
+            // Print numbers
+            for (int j = 0; j <= i; j++) {
+
+                System.out.print(num + " ");
+
+                num = num * (i - j) / (j + 1);
+            }
+
+            System.out.println();
+        }
+    }
+}
